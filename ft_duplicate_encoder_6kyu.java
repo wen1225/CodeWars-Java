8c4cb0cdeaab348abb4baa2c0de21e8764d81893
Ken Ho <46903916+wen1225@users.noreply.github.com>
2020-02-07 01:39:25 -0800

    Update ft_duplicate_encoder_6kyu.java

--- ft_duplicate_encoder_6kyu.java
+++ ft_duplicate_encoder_6kyu.java
@@ -1,29 +1,3 @@
-/*  
-Problem: 
-The goal of this exercise is to convert a string to a new string where each character 
-in the new string is "(" if that character appears only once in the original string, 
-or ")" if that character appears more than once in the original string. Ignore 
-capitalization when determining if a character is a duplicate.
-
-Examples:
-"din"      =>  "((("
-"recede"   =>  "()()()"
-"Success"  =>  ")())())"
-"(( @"     =>  "))((" 
-
-Problem Notes:
-Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
-
-Origin:
-6 Kyu, CodeWars.
-
-Notes:
-Can still be refactor to simplify logic. Check the other cleaner solution!
-
-Time to Solve:
-~4 hours. 2/6/20
-*/
-
 //My solution
 public class DuplicateEncoder {
 	static String encode(String word){
